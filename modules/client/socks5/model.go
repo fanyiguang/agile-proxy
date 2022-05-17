dd0bf650bae7209b69f5e676f432eec9ab6e39d1
pengjunwen <2397711850@qq.com>
2022-05-18 00:06:35 +0800

    初定启动配置文件

--- modules/client/socks5/model.go
+++ modules/client/socks5/model.go
@@ -1,10 +1,10 @@
 package socks5
 
 type Config struct {
-	Ip       string
-	Port     string
-	Username string
-	Password string
-	Type     string
-	Auth     int
+	Ip       string `json:"ip"`
+	Port     string `json:"port"`
+	Username string `json:"username"`
+	Password string `json:"password"`
+	Type     string `json:"type"`
+	Auth     int    `json:"auth"`
 }
